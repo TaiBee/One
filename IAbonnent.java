0e67226c98b43f1bc743cc2bf415459fb97879ab
forestPirate <133634749+forestPirate@users.noreply.github.com>
2023-05-15 09:18:37 +0200

    Add files via upload

---
+++ IAbonnent.java
@@ -0,0 +1,7 @@
+package Laba2;
+
+// Інтерфейс IAbonnent визначає поведінку об'єкту абонента,
+// яка включає метод для відображення інформації про абонента.
+public interface IAbonnent {
+    void display();
+}
